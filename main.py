69a6adeabe15472e733990297e0ac092b19af7b5
Maximilian <65408908+alativity@users.noreply.github.com>
2022-04-04 21:42:30 +0700

    Add crash handling system (V2)

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import vk
 import time
 import os
 
-if "token" in os.environ:
-    print("System Started")
-    session = vk.Session(access_token=os.environ.get('token'))
-    api = vk.API(session, v="5.95")
-    while True:
-        exit = api.account.setOnline(voip=0)
-        time.sleep(180)
-else:
-    print("Error: Token Missing")
-    exit()
+
+def starter():
+    try:
+        while True:
+            if "token" in os.environ:
+                print("System Started")
+                session = vk.Session(access_token=os.environ.get('token'))
+                api = vk.API(session, v="5.95")
+                while True:
+                    exit = api.account.setOnline(voip=0)
+                    time.sleep(180)
+            else:
+                raise Exception("Error: Token Missing")
+    except Exception:
+        print("Something crashed your program. Let's restart it")
+        starter()
+
+
+starter()
